517ff08cfa4675d69af4a297890e796e54009c6a
ernynk <62213559+ernynk@users.noreply.github.com>
2024-05-05 01:01:25 +0300

    Create app.py

---
+++ app.py
@@ -0,0 +1,17 @@
+import sys
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+@app.route('/')
+def hello():
+  try:
+    print("HHEELLOO")
+    sys.stdout.flush()
+    return '', 200
+  except:
+    return '', 200
+
+if __name__ == '__main__':
+    server_port = os.environ.get('PORT', '8080')
+    app.run(debug=True, port=server_port, host='0.0.0.0')
