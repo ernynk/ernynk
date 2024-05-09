d203cbe44cd9c2c235ef7545fd61c6e6d918a660
ernynk <62213559+ernynk@users.noreply.github.com>
2024-05-09 13:50:31 +0300

    Update comfy_vertex_app.py

--- comfy_vertex_app.py
+++ comfy_vertex_app.py
@@ -1,126 +1,138 @@
-import json
-import random, time, os, subprocess, sys
-from firebase_admin import firestore, credentials, initialize_app, storage
-from flask import Flask
-from flask import request as flask_request
-import requests
-
-cred = credentials.Certificate('scgbeta-1234-af5e222bb1fa.json')
-app = initialize_app(credential=cred)
-database = firestore.client()
-bucket = storage.bucket("scgbeta-1234.appspot.com")
-
-file_path = "workflow_api.json"
+import time, os, subprocess, sys, requests
+from flask import Flask, jsonify, request
 
 app = Flask(__name__)
 
+def check_server(url) -> bool:
+    try:
+        response = requests.get(url)
+        # Check if the response status code is in the 2xx range (indicating success)
+        if response.status_code // 100 == 2:
+            print(f"The server at {url} is up and running!")
+            return True
+        else:
+            print(f"The server at {url} returned a non-success status code: {response.status_code}")
+            return True
+    except requests.ConnectionError:
+        print(f"Failed to connect to the serverrrr at {url}. Is it running?")
+        return False
+    except Exception as e:
+        print(f"An error occurred: {e}")
+        return False
+            
+
 @app.route('/health', methods=['GET'])
 def health():
-    return '', 200
+    return 'OK', 200
 
-
+aa=requests.post(url="http://127.0.0.1:8080/predict", json={"instances":["start_main_server", "12345"],"parameters":{"parameter_key_1": "value"}})
 @app.route('/predict', methods=['POST'])
 def predict():
-    version = "V - 7"
+    data = request.get_json()
+    user_input = data["instances"][0]
+    # {"instances":["start_comfyui_server"],"parameters":{"parameter_key_1": "value"}}
+
     try:
-        print(version)
-        try:
-            print("input begin")
-            data = flask_request.get_json()
-            print(data)
-            print("inputttt")
-        except:
-            print("NO dataa")
-            
-        config_document = database.document("users/config").get()
-        command_list = eval(config_document.get("command_list"))
-        file_no = int(config_document.get("file_no"))
-        print(command_list)
 
-
-        def check_server(url) -> bool:
-            try:
-                response = requests.get(url)
-                # Check if the response status code is in the 2xx range (indicating success)
-                if response.status_code // 100 == 2:
-                    print(f"The server at {url} is up and running!")
-                    return True
-                else:
-                    print(f"The server at {url} returned a non-success status code: {response.status_code}")
-                    return True
-            except requests.ConnectionError:
-                print(f"Failed to connect to the server at {url}. Is it running?")
-                return False
-            except Exception as e:
-                print(f"An error occurred: {e}")
-                return False
-            
-        comfyUI_up = check_server("http://127.0.0.1:8188")
-        if comfyUI_up:
-            print("uppp")
-        else:
-            internal_server_process = subprocess.Popen(command_list)
-            time.sleep(30)
-            print("slepttt")
-
-
-            # Open the file in read mode
-        with open(file_path, "r") as file:
-            # Load the JSON data
-            prompt_text = json.load(file)
-        print("readd")
-
-        prompt_text["12"]["inputs"]["color"] = random.randint(1000000, 16777215)
-        print("changed")
-
-        def queue_prompt(prompt):
-            # URL of the endpoint to which you want to send the POST request
-            url = "http://127.0.0.1:8188/prompt"
-
-            # Data to send in the POST request (as JSON)
-            data =  {"prompt": prompt}
-
-            # Send POST request
-            response = requests.post(url, json=data)
-
-            # Check if request was successful
-            if response.status_code == 200:
-                response_data = response.json()  # Convert response JSON to dictionary
-                print("POST request sent successfully!")
-                print("Response:", response_data)
-                return "POST request sent successfully! Response: " + str(response_data)
+        if user_input == "start_comfyui_server":
+            is_comfyUI_up = check_server("http://127.0.0.1:8188")
+            if is_comfyUI_up:
+                print("comfyui was already up")
+                sys.stdout.flush()
+                return jsonify("comfyui is already up")
             else:
-                print(f"Failed to send POST request. Status code: {response.status_code}")
-                return f"Failed to send POST request. Status code: {response.status_code}"
+                comfyui_startup_commands = data["instances"][1]
+                global comfyui_process
+                comfyui_process = subprocess.Popen(comfyui_startup_commands)
+                time.sleep(30)
+                print("comfyui server started")
+                sys.stdout.flush()
+                return jsonify("comfyui server started")
+            
 
-        queue_prompt(prompt_text)
-        print("queued")
-        print(os.listdir("./output"))
-        print("osssssss")
-        blob = bucket.blob(str(round(time.time(),2)))
-        print("listing dirrr")
-        directories = [d for d in os.listdir("./output")]
-        for directory in directories:
-            print(directory)
-        print("listed dirrr")
-        print(str("./output/" + sorted(os.listdir("./output"))[file_no]))
-        blob.upload_from_filename(filename=str("./output/" + sorted(os.listdir("./output"))[file_no]))
-        print("uploaded")
+        if user_input == "start_main_server":
+            is_comfyUI_up = check_server("http://127.0.0.1:8888/health")
+            if is_comfyUI_up:
+                print("main server was already up")
+                sys.stdout.flush()
+                return jsonify("main server is already up")
+            else:
+                main_server_startup_commands = ["python", "main_app.py"]
+                global main_server_process
+                main_server_process = subprocess.Popen(main_server_startup_commands, stdout=subprocess.DEVNULL,shell=True)
+                time.sleep(3)
+                print("main server server started")
+                sys.stdout.flush()
+                return jsonify("main server server started")
+            
 
-        sys.stdout.flush()
-        return 'done', 200
-    except Exception as err:
-        print(version)
-        print("EXCEPTTTT")
-        print(err)
-        print("EXCEPTTTT")
-        sys.stdout.flush()
-        return 'err', 200
+        if user_input == "shutdown_comfyui_server":
+            if check_server("http://127.0.0.1:8188"):
+                comfyui_process.terminate()
+                print("comfyui shut down")
+                sys.stdout.flush()
+                return jsonify("shutdown_comfy"), 200
+            else:
+                print("comfyui was already down")
+                sys.stdout.flush()
+                return jsonify("comfyui was already down"), 200
+            
+
+        if user_input == "shutdown_main_server":
+            if check_server("http://127.0.0.1:8888/health"):
+                # main_server_process.terminate()
+                requests.get("http://127.0.0.1:8888/shutdown")
+                print("main server shut down")
+                sys.stdout.flush()
+                return jsonify("shutdown_main_server"), 200
+            else:
+                print("main server was already down")
+                sys.stdout.flush()
+                return jsonify("main server is already down"), 200
+
+
+        if user_input == "replace_file":
+            github_link = data["instances"][1]
+            file_path = data["instances"][2]
+            command = f"""sh -c 'curl -o {file_path} {github_link}'"""
+            print("COMMAND: " + command)
+            replace_file_result = subprocess.run(command, shell=True)
+            sys.stdout.flush()
+            return jsonify(replace_file_result.stdout), 200
         
+        
+        if user_input == "run_custom_code":
+            user_code = data["instances"][1]
+            eval(user_code)
+            return jsonify("Code ran"), 200
+
+
+        if user_input == "predict":
+            prediction_input = data["instances"][1]
+            response = requests.post(url="http://127.0.0.1:8888/predict", json={"input": prediction_input})
+            return response.json(), 200
+        
+        if user_input == "check_main_server":
+            if check_server("http://127.0.0.1:8888/health"):
+                print("main server upp")
+                sys.stdout.flush()
+                return jsonify("main is uppp"), 200
+            else:
+                print("main server downn")
+                sys.stdout.flush()
+                return jsonify("main server downnn"), 200
+        
+        return jsonify({"no_match": user_input})
+        
+
+    except Exception as error:
+        print("Exception happened")
+        print(error)
+        print("Exception written")
+        return jsonify("exceptionn"), 200
 
 if __name__ == '__main__':
     server_port = os.environ.get('PORT', '8080')
-    app.run(debug=True, host='0.0.0.0', port=server_port)
-
+    app.run(debug=True, host='127.0.0.1', port=server_port)
 
 
